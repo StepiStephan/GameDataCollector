7f6676552ba7630471247bf1818930c6cb82c681
Stephan Kusch <stephan.wolfgang.kusch@mni.thm.de>
2021-09-28 20:04:32 +0200

    tests fur speichern und laden hinzugefügt

---
+++ DataManaging.Test/DataManagerTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace DataManaging.Test
+{
+    public class DataManagerTest
+    {
+        DataManager dataManager;
+        [SetUp]
+        public void Setup()
+        {
+            dataManager = new DataManager();
+        }
+
+        [Test]
+        public void AddKonsoleTest()
+        {
+        }
+        [Test]
+        public void AddStorageTest()
+        {
+
+        }
+    }
+}
--- DataManaging.Test/UnitTest1.cs
+++ DataManaging.Test/UnitTest1.cs
@@ -1,17 +1,59 @@
 using NUnit.Framework;
+using System.Collections.Generic;
 
 namespace DataManaging.Test
 {
-    public class Tests
+    
+    public class DataLoaderTests
     {
+        DataLoader<List<string>> dataLoader;
+        DataSaver<List<string>> dataSaver;
+        List<string> testList;
         [SetUp]
         public void Setup()
         {
+            dataLoader = new DataLoader<List<string>>("TestLoader");
+            dataSaver = new DataSaver<List<string>>("TestLoader");
+            testList = new List<string>()
+            {
+                "das",
+                "ist",
+                "ein",
+                "Test",
+                "das",
+                "ist",
+                "ein",
+                "Test",
+                "das",
+                "ist",
+                "ein",
+                "Test",
+                "das",
+                "ist",
+                "ein",
+                "Test"
+            };
         }
 
         [Test]
-        public void Test1()
+        public void LengthTest()
         {
+            dataSaver.SaveObject(testList);
+            var resultList = dataLoader.LoadObject();
+            Assert.AreEqual(testList.Count, resultList.Count);
+        }
+        [Test]
+        public void ContentSameTest()
+        {
+            dataSaver.SaveObject(testList);
+            var resultList = dataLoader.LoadObject();
+            for(int i = 0; i < resultList.Count; i++)
+            {
+                if(resultList[i] != testList[i])
+                {
+                    Assert.Fail();
+                }
+            }
             Assert.Pass();
         }
     }
